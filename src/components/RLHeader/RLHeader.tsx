2f83df13e702b392be60d83d05fd3115283b3ee2
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-06-26 18:07:23 -0500

    feat: set image in rl-header

--- src/components/RLHeader/RLHeader.tsx
+++ src/components/RLHeader/RLHeader.tsx
@@ -6,7 +6,9 @@ export const RLHeader = ({ className }: { className?: string }) => {
       className={`py-4 px-4 lg:px-8 flex items-center justify-between bg-gray-800 text-white ${className}`}
     >
       <img
-        src={require("../../public/ratherlabs-logo.jpeg")}
+        src={
+          "https://miro.medium.com/v2/resize:fill:176:176/1*dlaXtsXy8vktxwgWs4tg4g.jpeg"
+        }
         className="w-12 h-12"
       />
       <div>
