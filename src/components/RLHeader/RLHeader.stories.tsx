30cf9c3188b9960d8cda08cc6dc5f282739f07aa
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-07-03 10:01:27 -0500

    feat: children prop was added

--- src/components/RLHeader/RLHeader.stories.tsx
+++ src/components/RLHeader/RLHeader.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
 import { RLHeader } from "./RLHeader";
 
 const meta: Meta<typeof RLHeader> = {
@@ -9,6 +11,9 @@ const meta: Meta<typeof RLHeader> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const MyChildren = () => <div>:)</div>;
 export const Basic: Story = {
-  args: {},
+  args: {
+    children: <MyChildren />,
+  },
 };
--- src/components/RLHeader/RLHeader.tsx
+++ src/components/RLHeader/RLHeader.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
-export const RLHeader = ({ className }: { className?: string }) => {
+export const RLHeader = ({
+  children,
+  className,
+}: {
+  children: JSX.Element;
+  className?: string;
+}) => {
   return (
     <div
-      className={`py-4 px-4 lg:px-8 w-full flex items-center justify-between bg-gray-800 text-white ${className}`}
+      className={`py-4 px-4 lg:px-8 w-full flex items-center justify-between
+      bg-gray-800 text-white ${className}`}
     >
-      <img
-        src={
-          "https://miro.medium.com/v2/resize:fill:176:176/1*dlaXtsXy8vktxwgWs4tg4g.jpeg"
-        }
-        className="w-12 h-12"
-      />
-      <div>
+      <div className="flex items-center space-x-2">
+        <img
+          src={
+            "https://miro.medium.com/v2/resize:fill:176:176/1*dlaXtsXy8vktxwgWs4tg4g.jpeg"
+          }
+          className="w-12 h-12"
+        />
         <div className="text-base lg:text-lg font-normal">
           Membrane Challenge
         </div>
       </div>
+      <>{children}</>
     </div>
   );
 };
